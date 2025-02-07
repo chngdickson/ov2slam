861e08422cb7db0b8cfb8e12abb4eaeda866b74c
chngdickson <chngdickson@gmail.com>
2025-02-07 21:29:14 +0800

    wefwef

--- src/my_publisher.cpp
+++ src/my_publisher.cpp
@@ -43,9 +43,8 @@ class ExampleRosClass{
         ros::NodeHandle _nh;
         message_filters::Subscriber<sensor_msgs::Image> _depth_sub; 
         message_filters::Subscriber<sensor_msgs::Image> _rgb_sub;
-        // using mysyncpolicy = sync_policies::ApproximateTime<Image, CameraInfo>;
         using ExactSyncPolicy = sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image>;
-        std::shared_ptr<message_filters::Synchronizer<ExactSyncPolicy> > _sync;
+        std::shared_ptr<message_filters::Synchronizer<ExactSyncPolicy>> _sync;
         ros::Publisher depth_new_pub;
     
     public:
@@ -60,8 +59,8 @@ class ExampleRosClass{
             _depth_sub.subscribe(_nh, depth_topic, 1);
             _rgb_sub.subscribe(_nh, rgb_topic, 1);
             _sync = std::make_shared<message_filters::Synchronizer<ExactSyncPolicy>>(10);
-            _sync->connectInput(_depth_sub, _rgb_sub);
-            _sync->registerCallback(boost::bind(&ExampleRosClass::subscriberCallback, this, _1, _2));
+            _sync.connectInput(_depth_sub, _rgb_sub);
+            _sync.registerCallback(boost::bind(&ExampleRosClass::subscriberCallback, this, _1, _2));
         } 
         void initializePublishers(std::string depth_topic_new)
         {

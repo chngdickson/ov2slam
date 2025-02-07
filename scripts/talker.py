066c2797fe97b308c4dcbffd0e3beac964b2d461
chngdickson <chngdickson@gmail.com>
2025-02-08 03:59:55 +0800

    fewfwefe

--- scripts/talker.py
+++ scripts/talker.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
 import rospy
 import torch
-from sensor_msgs.msg import Image
+import tf
+from sensor_msgs.msg import Image, CameraInfo, PointCloud2
+import message_filters
 
-img_pub = rospy.Publisher('/cam0/image_raw', Image, queue_size=10)
-I = 0
-def imageCallback(img_data):
-    global I
-    if I % 2:
-        pub_data = Image()
-        pub_data.data = img_data.data
-        pub_data.header = img_data.header
-        pub_data.height = img_data.height
-        pub_data.width = img_data.width
-        pub_data.encoding = img_data.encoding
-        pub_data.is_bigendian = img_data.is_bigendian
-        pub_data.step = img_data.step
-        img_pub.publish(pub_data)
-    I=I+1
+
+class CarlaSyncListener:
+    def __init__(self):
+        self.image_sub = message_filters.Subscriber('carla/ego_vehicle/rgb_back/image', Image)
+        self.info_sub = message_filters.Subscriber('carla/ego_vehicle/rgb_back/camera_info', CameraInfo)
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.info_sub], 1, 1) # Changed code
+        self.ts.registerCallback(self.callback)
+
+    def callback(self, image, camera_info):
+        print("done")
     
-def talker():
-    rospy.init_node('camera_listener')
-    rospy.Subscriber("/carla/ego_vehicle/rgb_back_right/image", Image, imageCallback)
-    rospy.spin()
-
 if __name__ == '__main__':
-    print(torch.cuda.is_available())
+    csl = CarlaSyncListener()
+    rospy.init_node("sample_message_filters", anonymous=True)
     try:
-        talker()
+        rospy.spin()
     except rospy.ROSInterruptException:
-        pass
+        print("shutting_down")

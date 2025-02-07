279137519fcfa554eb412df177201dedf2458c42
chngdickson <chngdickson@gmail.com>
2025-02-08 06:07:47 +0800

    fwefwefwe

--- scripts/talker.py
+++ scripts/talker.py
@@ -11,8 +11,8 @@ import message_filters
 class CarlaSyncListener:
     def __init__(self, topic_pose):
         self.image_sub = message_filters.Subscriber("carla/ego_vehicle/rgb_{topic_pose}/image", Image)
-        self.info_sub = message_filters.Subscriber(f"carla/ego_vehicle/rgb_{topic_pose}/camera_info", CameraInfo)
-        self.depth_sub = message_filters.Subscriber(f"carla/ego_vehicle/depth_{topic_pose}/image", Image)
+        self.info_sub = message_filters.Subscriber("carla/ego_vehicle/rgb_{topic_pose}/camera_info", CameraInfo)
+        self.depth_sub = message_filters.Subscriber("carla/ego_vehicle/depth_{topic_pose}/image", Image)
         self.ts = message_filters.TimeSynchronizer([self.image_sub, self.info_sub, self.depth_sub], 10)
         self.ts.registerCallback(self.callback)
         self.timestampedInfo = OrderedDict() #Timestamp:combined pointcloud

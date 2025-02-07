3be1df0a936406808c57e5744f118c7560276283
chngdickson <chngdickson@gmail.com>
2025-02-07 19:46:29 +0800

    fwefwe

--- src/my_publisher.cpp
+++ src/my_publisher.cpp
@@ -69,7 +69,7 @@ class ExampleRosClass{
             const sensor_msgs::Image::ConstPtr& depth_cam,
             const sensor_msgs::Image::ConstPtr& rgb_cam)
         {
-            depth_new = new sensor_msgs::Image;
+            depth_cam.frame_id = rgb_cam.frame_id;
         }
 };
 

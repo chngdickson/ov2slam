d72f990b36306dfd5886397bb4a7a59bcce303bd
chngdickson <chngdickson@gmail.com>
2025-02-07 20:15:42 +0800

    gergerge

--- src/my_publisher.cpp
+++ src/my_publisher.cpp
@@ -53,8 +53,8 @@ class ExampleRosClass{
 
         void initializeSubscribers(std::string depth_topic, std::string rgb_topic)
         {
-            depth_sub.subscribe(this, depth_topic,10);
-            rgb_sub.subscribe(this, rgb_topic, 10);
+            depth_sub.subscribe(this, depth_topic);
+            rgb_sub.subscribe(this, rgb_topic);
             message_filters::TimeSynchronizer <sensor_msgs::Image, sensor_msgs::Image> ros_sync(
                 depth_sub, rgb_sub, 10
                 ); 

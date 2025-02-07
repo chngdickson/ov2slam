3deb634af39d51760fde5f6f88646e0ea48e2e55
chngdickson <chngdickson@gmail.com>
2025-02-07 21:22:36 +0800

    efwef

--- src/my_publisher.cpp
+++ src/my_publisher.cpp
@@ -58,7 +58,7 @@ class ExampleRosClass{
             depth_sub.subscribe(nh_, depth_topic, 10);
             rgb_sub.subscribe(nh_, rgb_topic, 10);
             _sync = std::make_shared<message_filters::Synchronizer<ExactSyncPolicy> >(10);
-            _sync->connectInput(_rgb_sub, _camera_info_sub);
+            _sync->connectInput(depth_sub, rgb_sub);
             _sync->registerCallback(boost::bind(&ExampleRosClass::subscriberCallback, this, _1, _2));
         } 
         void initializePublishers(std::string depth_topic_new)

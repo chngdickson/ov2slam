69fdd003b2cff9489dcd9b09823f8830dfabe791
chngdickson <chngdickson@gmail.com>
2025-02-08 06:07:21 +0800

    gegeg

--- scripts/talker.py
+++ scripts/talker.py
@@ -10,7 +10,7 @@ import message_filters
 
 class CarlaSyncListener:
     def __init__(self, topic_pose):
-        self.image_sub = message_filters.Subscriber(str(f"carla/ego_vehicle/rgb_{topic_pose}/image"), Image)
+        self.image_sub = message_filters.Subscriber("carla/ego_vehicle/rgb_{topic_pose}/image", Image)
         self.info_sub = message_filters.Subscriber(f"carla/ego_vehicle/rgb_{topic_pose}/camera_info", CameraInfo)
         self.depth_sub = message_filters.Subscriber(f"carla/ego_vehicle/depth_{topic_pose}/image", Image)
         self.ts = message_filters.TimeSynchronizer([self.image_sub, self.info_sub, self.depth_sub], 10)

eb826517e348c770977560b1f05ea203ac52b759
chngdickson <chngdickson@gmail.com>
2025-02-08 16:50:46 +0800

    ewfwefweef

--- scripts/talker.py
+++ scripts/talker.py
@@ -76,7 +76,8 @@ class ManySyncListener:
         RGB color of an array.
         "max_depth" is used to omit the points that are far enough.
         """
-        print(K)
+        K = np.array(K)
+        K.reshape((3,3))
         far = 1000.0  # max depth in meters.
         w,h,K = int(w), int(h), np.array(K)
         pixel_length = w*h

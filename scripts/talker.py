130b0a12760b4e4392ee1ca04d0b76834d137916
chngdickson <chngdickson@gmail.com>
2025-02-13 01:03:37 +0800

    fewfweef

--- scripts/talker.py
+++ scripts/talker.py
@@ -218,8 +218,8 @@ class ManySyncListener:
         K4x4 = self.K3x3to4x4(torch.tensor(K_ros).reshape((3,3))).to(device=device, dtype=dtype)
         
         M_Basis_Cam2W = torch.tensor([
+                            [ 0, 1, 0, 0],
                             [ 1, 0, 0, 0],
-                            [ 0, -1, 0, 0],
                             [ 0, 0, -1, 0],
                             [ 0, 0, 0, 1]], dtype=dtype, device=device)
         
